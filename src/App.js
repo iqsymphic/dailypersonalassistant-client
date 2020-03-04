36c369c164edc0ff0eb6e8eb126430f2776121f8
iqsymphic <iqsymphic@gmail.com>
2020-03-03 22:38:11 -0500

    rebuild

---
+++ src/App.js
@@ -0,0 +1,126 @@
+import React, { Component } from "react";
+import Header from "./components/Header";
+import AddInput from "./components/AddInput";
+import List from "./components/List";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fetchDataLoaded: false,
+      fetchData: [],
+      content: ""
+    };
+    this.submitForm = this.submitForm.bind(this);
+    this.deleteForm = this.deleteForm.bind(this);
+    this.editForm = this.editForm.bind(this);
+    this.editChecked = this.editChecked.bind(this);
+    this.reOrderList = this.reOrderList.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchAllData();
+  }
+
+  fetchAllData() {
+    fetch("/api/todolist")
+      .then(response => {
+        return response.json();
+      })
+      .then(responseJson => {
+        this.setState(prevState => {
+          return {
+            fetchDataLoaded: true,
+            fetchData: responseJson.data.todolist
+          };
+        });
+      });
+  }
+
+  deleteForm(id) {
+    fetch(`/api/todolist/${id}`, {
+      method: "DELETE"
+    }).then(response => {
+      if (response.status === 200) {
+        this.fetchAllData();
+      }
+    });
+  }
+
+  editForm(e, id) {
+    fetch(`/api/todolist/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        content: e.target.content.value,
+        checked: e.target.checked.value
+      })
+    }).then(response => {
+      this.fetchAllData();
+    });
+  }
+
+  editChecked(id, content, checked) {
+    fetch(`/api/todolist/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        content: content,
+        checked: checked
+      })
+    }).then(response => {
+      this.fetchAllData();
+    });
+  }
+
+  reOrderList(itemsId) {
+    fetch(`/api/todolist/reorderlist`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        itemsId: itemsId
+      })
+    });
+  }
+
+  submitForm(e) {
+    fetch("/api/todolist", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        content: e.target.content.value
+      })
+    }).then(response => {
+      if (response.status === 200) {
+        this.fetchAllData();
+      }
+    });
+    e.target.content.value = "";
+  }
+
+  fetchComplete() {
+    if (this.state.fetchDataLoaded) {
+      return (
+        <List
+          fetchData={this.state.fetchData}
+          deleteForm={this.deleteForm}
+          editForm={this.editForm}
+          editChecked={this.editChecked}
+          reOrderList={this.reOrderList}
+        />
+      );
+    } else return <p className="loading">Now Loading....</p>;
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <Header />
+        <AddInput submitForm={this.submitForm} />
+        {this.fetchComplete()}
+      </div>
+    );
+  }
+}
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="notepad">
+        <h1>Daily Activity</h1>
+      </div>
+    );
+  }
+}
